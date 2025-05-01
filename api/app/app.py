0f273e6ef4b3c6943231d29c01d3a86619a6fe36
Nagappan Subramoni <nagappans@gmail.com>
2025-05-01 15:39:09 +0530

    Comitting with prometheus interface

--- api/app/app.py
+++ api/app/app.py
@@ -1,15 +1,27 @@
 import gradio as gr
 import numpy as np
 import joblib
+from prometheus_client import Counter, start_http_server
+
+# app runs on: http://localhost:8000
+
+# Prometheus metrics available at: http://localhost:8001/metrics
+
+# Start Prometheus metrics server on port 8001
+start_http_server(8001)
+
+# Define Prometheus counter
+PREDICTION_COUNTER = Counter('death_event_predictions_total', 'Total number of predictions made')
 
 save_file_name = 'xgboost-model.pkl'  # Path to the saved model
 model = joblib.load(save_file_name)
 
-# Dummy prediction function (replace with your model logic)
-
 def predict_death_event(age, anaemia, creatinine_phosphokinase, diabetes,
        ejection_fraction, high_blood_pressure, platelets,
        serum_creatinine, serum_sodium, sex, smoking, time):
+    
+    PREDICTION_COUNTER.inc()  # Increment the counter each time this function is called
+    
     input_array = np.array([age, anaemia, creatinine_phosphokinase, diabetes,
        ejection_fraction, high_blood_pressure, platelets,
        serum_creatinine, serum_sodium, sex, smoking, time]).reshape(1, -1)
@@ -19,9 +31,7 @@ def predict_death_event(age, anaemia, creatinine_phosphokinase, diabetes,
     else:
         return "Patient survived during the follow-up period"
 
-
-
-# Interface
+# Gradio interface
 iface = gr.Interface(
     fn=predict_death_event,
     inputs=[
@@ -43,8 +53,4 @@ iface = gr.Interface(
     description="Enter patient details to predict a risk score."
 )
 
-# iface.launch(server_port=8000)
-# iface.launch(server_name="0.0.0.0", server_port=8000)
 iface.launch(server_name="0.0.0.0", server_port=8000, share=False, inbrowser=False)
-
-
